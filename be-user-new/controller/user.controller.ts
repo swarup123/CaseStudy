e3e4de5c2efaf1eb6e1aa77d98f0e531c0e9b7ca
swarup123 <swarup.mis07@gmail.com>
2023-12-04 01:05:47 +0530

    Update user.controller.ts

--- be-user-new/controller/user.controller.ts
+++ be-user-new/controller/user.controller.ts
@@ -9,7 +9,7 @@ export const createUser = async (req: Request, res: Response) => {
     if(firstName.length > 100 || lastName.length > 100) {
       return res.send({
         statusCode: 400,
-        error: "Maximum 100 character allowed in first name and last name ",
+        errorMessage: "Maximum 100 character allowed in first name and last name ",
       });
     }
     
