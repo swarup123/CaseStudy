c0f741aa803c5ad314209b5a3f84086a47a31641
swarup123 <swarup.mis07@gmail.com>
2023-12-03 15:50:54 +0530

    Delete fe-user directory

--- fe-user/src/constant.type.ts
+++
@@ -1,4 +0,0 @@
-export const API_LIST_USER: string = "http://localhost:8000/api/v1/list-users";
-export const API_CREATE_USER: string =
-  "http://localhost:8000/api/v1/create-user";
-export const MAX_LENGTH = 100;
